366d075ec5b7f08314c6b209fc64442556f7506a
krishna cheashty <krishnacheashty@gmail.com>
2026-03-08 03:13:43 +0600

    Add TASK 01 CONTINUOUS

--- Learn_SQL_CTE.sql
+++ Learn_SQL_CTE.sql
@@ -8,13 +8,16 @@ WITH CTE_TOTAL_SALES AS
 SELECT 
 	CustomerID,
 	SUM(Sales) AS TotalSales
-FROM Sales.Orders
+FROM Sales.Orders 
 GROUP BY CustomerID
 )
 
 --MAIN QUERY
 SELECT
 C.CustomerID,
-C.FirstName, 
+C.FirstName,
+C.LastName,
+cts.TotalSales
 FROM Sales.Customers C
-LEFT JOIN CTE_TOTAL_SALES
+LEFT JOIN CTE_TOTAL_SALES cts
+ON cts.CustomerID=C.CustomerID
