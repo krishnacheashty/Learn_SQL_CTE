698d319ff8388d6377f2d8f7bfdaa213627b7e0d
krishna cheashty <krishnacheashty@gmail.com>
2026-03-09 03:10:42 +0600

    Added task 02

--- recursive_CTR.sql
+++ recursive_CTR.sql
@@ -20,6 +20,41 @@ WITH Series AS (
 --main query
 
 SELECT *
-FROM Series
+FROM Series;
 --OPTION(MAXRECURSION 10 )
 -- DEFAULT MAXRECURSION 100,IF WE INCREASE IT BY OPTION(MAXRECURSION 5000) THEN DEFAULT VALUE INCREASE IN 1000.
+
+
+								-- Task 02
+	-- show the employee hierarchy by displaying each employee's 
+	--level within the organization.
+WITH CTE_Level_Organization AS
+	(
+		-- Anchor query
+		SELECT 
+			EmployeeID,
+			FirstName,
+			ManagerID,
+			1 AS Level
+		FROM Sales.Employees
+		WHERE ManagerID IS NULL
+		UNION ALL 
+		--RECURSIVE QUERY
+		SELECT
+			E.EmployeeID,
+			E.FirstName,
+			E.ManagerID,
+			Level + 1
+		FROM Sales.Employees AS E
+		INNER JOIN CTE_Level_Organization clo
+		ON E.ManagerID=clo.EmployeeID
+
+	)
+
+	--MAIN QUERY
+
+SELECT 
+*
+FROM CTE_Level_Organization
+
+
