315491bbaed5fe8e2bc07eeee59c4eb5e674f6ec
krishna cheashty <krishnacheashty@gmail.com>
2026-03-09 01:45:52 +0600

    Added tast 3 rank customers based on total sales per customer.

--- Learn_SQL_CTE.sql
+++ Learn_SQL_CTE.sql
@@ -44,7 +44,19 @@ SELECT
 FROM Sales.Orders
 GROUP BY CustomerID
 )
+--step # 3 rank customers based on total sales per customer.
+,CTE_Customer_Rank AS
+(
+SELECT
+CustomerID,
+TotalSales,
+RANK() OVER(ORDER BY TotalSales DESC) AS CustomerRank
+FROM CTE_TOTAL_SALES
+)
+-- step4 : 
+,c
 
+--SELECT * FROM CTE_Customer_Rank
 --MAIN QUERY
 
 SELECT
@@ -52,11 +64,22 @@ C.CustomerID,
 C.FirstName,
 C.LastName,
 cts.TotalSales,
-clo.LAST_ORDER_DATE
+clo.LAST_ORDER_DATE,
+ccr.CustomerRank
 FROM Sales.Customers C
 LEFT JOIN CTE_TOTAL_SALES cts
 ON cts.CustomerID=C.CustomerID
 LEFT JOIN CTE_Last_order clo
 ON clo.CustomerID=C.CustomerID
+LEFT JOIN CTE_Customer_Rank ccr
+ON ccr.CustomerID=C.CustomerID
 
+
+--							NESTED CTE 
+
+--CTE inside another CTE.A nested CTE uses the result of another CTE,
+--so it can't run indeperdently.
+
+
+ 
 
